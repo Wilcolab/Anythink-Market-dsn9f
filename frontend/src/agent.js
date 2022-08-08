168a2645fe3beca7e1444534c54bb9dafe7d073c
Nir Alfasi <alfasin@gmail.com>
2022-08-09 02:09:28 +0300

    move out to a variable

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -3,10 +3,11 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
+const defaultBackendRoot = process.env.BACKEND_URL || "https://api.anythink.market/api";
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : process.env.BACKEND_URL || "https://api.anythink.market/api";
+    : defaultBackendRoot;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
