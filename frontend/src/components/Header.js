8cc6aa1daaac108f03c198dd5448d8c788afa86c
Nir Alfasi <alfasin@gmail.com>
2022-10-26 15:05:30 +0300

    move from class to component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,55 +2,61 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/topbar_logo.png";
 
-const LoggedOutView = () => {
-  return (
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
-        <Link to="/login" className="nav-link">
-          Sign in
-        </Link>
-      </li>
+const LoggedOutView = (props) => {
+  if (!props.currentUser) {
+    return (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </li>
 
-      <li className="nav-item">
-        <Link to="/register" className="nav-link">
-          Sign up
-        </Link>
-      </li>
-    </ul>
-  );
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Sign up
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+  return null;
 };
 
 const LoggedInView = (props) => {
-  return (
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
-        <Link to="/editor" className="nav-link">
-          <i className="ion-compose"></i>&nbsp;New Item
-        </Link>
-      </li>
+  if (props.currentUser) {
+    return (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link to="/editor" className="nav-link">
+            <i className="ion-compose"></i>&nbsp;New Item
+          </Link>
+        </li>
 
-      <li className="nav-item">
-        <Link to="/settings" className="nav-link">
-          <i className="ion-gear-a"></i>&nbsp;Settings
-        </Link>
-      </li>
+        <li className="nav-item">
+          <Link to="/settings" className="nav-link">
+            <i className="ion-gear-a"></i>&nbsp;Settings
+          </Link>
+        </li>
 
-      <li className="nav-item">
-        <Link to={`/@${props.currentUser.username}`} className="nav-link">
-          <img
-            src={props.currentUser.image}
-            className="user-pic pr-1"
-            alt={props.currentUser.username}
-          />
-          {props.currentUser.username}
-        </Link>
-      </li>
-    </ul>
-  );
+        <li className="nav-item">
+          <Link to={`/@${props.currentUser.username}`} className="nav-link">
+            <img
+              src={props.currentUser.image}
+              className="user-pic pr-1"
+              alt={props.currentUser.username}
+            />
+            {props.currentUser.username}
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
+  return null;
 };
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
     return (
       <nav
         className="navbar navbar-expand-md navbar-dark"
@@ -60,14 +66,11 @@ class Header extends React.Component {
           <img alt="logo" src={logo} />
         </Link>
 
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
-        ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
-        )}
+        <LoggedOutView currentUser={this.props.currentUser} />
+
+        <LoggedInView currentUser={this.props.currentUser} />
       </nav>
     );
-  }
 }
 
 export default Header;
